1a80be572ee0bbec95b3bb5dc76ffa5d145f161a
Mohammad Arabzadeh <mrbzadhoo82@gmail.com>
2023-08-02 17:06:02 +0430

    decorate and safety check added

--- pages/safety.vue
+++ pages/safety.vue
@@ -1,40 +1,79 @@
 <template>
-  <v-app>
-    <div
-      class="ma-4"
-      style="height: 100%"
+  <div>
+   <decoration />
+  <div style="margin: 80px;">
+    <p>😊 please insert your requirements.txt content so we can tell your vulnerability: <br> here I provide you a sample file you can click submit saftey check to find out more 👇</p>
+  <editor
+  v-model="content"
+api-key='your-api-key' :init="{ menubar: false,
+        toolbar: false,/* your other settings */ }" />
+
+  <div
+    @click="submitSafetyCheck"
+    style="display: flex; 
+    justify-content: center;
+    align-items: center;
+    margin: 40px;">
+    <v-btn
+    rounded
+    color="primary"
+    dark
     >
-      <v-calendar
-        :events="events"
-        now="2022-04-17"
-      ></v-calendar>
-    </div>
-  </v-app>
+    
+      Submit safety Check
+  
+    </v-btn>
+</div>
+<div
+style="display: flex; 
+  justify-content: center;
+  align-items: center;">
+  <pre>{{ response }}</pre>
+</div>
+</div>
+</div>
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue'
+import axios from 'axios'
+import Decoration from '~/components/Forms/Decoration'
+
+
 export default {
-  name: 'App',
   data: () => {
     return {
-      events: [
-        {
-          name: 'Event 1',
-          start: '2023-08-02',
-          timed: false
-        },
-        {
-          name: 'Event 2',
-          start: '2023-08-04',
-          end: '2022-04-07'
-        },
-        {
-          name: 'Event 3',
-          start: '2023-08-05T08:00:00',
-          end: '2022-04-09T10:00:00',
-          timed: true
-        }
-      ]
+      content: '<p>insecure-package==0.1.0</p> \n <p>Django==1.0</p>',
+      response: ''
+    }
+  },
+  name: 'App',
+  components: {
+    editor: Editor,
+    decoration:  Decoration
+  },
+  methods: {
+    async submitSafetyCheck() {
+      try {
+        let enhancedContent = this.content.replaceAll('<p>', '')
+        enhancedContent = enhancedContent.replaceAll('</p>', '')
+        const response = await axios.post(
+          'https://securise-backend.runflare.run/safety-check/',
+          {
+            // requirements: this.content
+            requirements: enhancedContent
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        console.log(response.data)
+        this.response = response.data
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
